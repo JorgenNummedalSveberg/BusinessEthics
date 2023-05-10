b9f94584f3ce81624a450368e7231e7dad2fb2b9
Jørgen Nummedal Sveberg <jorgenns@stud.ntnu.no>
2023-05-10 12:55:39 +0200

    websocket testing

---
+++ api/websocket.js
@@ -0,0 +1,33 @@
+import { Server } from 'ws';
+
+export default async function(req, res) {
+    const wss = new Server({ noServer: true });
+
+    wss.on('connection', (ws) => {
+        console.log('WebSocket connection established.');
+
+        ws.send(JSON.stringify({
+            type: 'welcome',
+            message: 'WebSocket connection established.'
+        }));
+
+        ws.on('message', (message) => {
+            const messageData = JSON.parse(message);
+            console.log(messageData); // Process the message data as needed
+
+            // Send a response back to the client if needed
+            ws.send(JSON.stringify({
+                type: 'response',
+                message: 'Message received and processed.'
+            }));
+        });
+
+        ws.on('close', () => {
+            console.log('WebSocket connection closed.');
+        });
+    });
+
+    wss.handleUpgrade(req, req.socket, Buffer.alloc(0), (ws) => {
+        wss.emit('connection', ws, req);
+    });
+}
--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,40 @@
 <script>
-	export let name;
+    let ws;
+
+    function connectWebsocket() {
+        ws = new WebSocket('wss:/api/websocket');
+
+        ws.addEventListener('open', (event) => {
+            console.log('WebSocket connection established.');
+        });
+
+        ws.addEventListener('message', (event) => {
+            const messageData = JSON.parse(event.data);
+            console.log(messageData); // Process the message data as needed
+        });
+
+        ws.addEventListener('close', (event) => {
+            console.log('WebSocket connection closed.');
+        });
+
+        ws.addEventListener('error', (event) => {
+            console.error('WebSocket error:', event.error);
+        });
+    }
+
+    function disconnectWebsocket() {
+        ws.close();
+    }
+
+    function sendMessage() {
+        const messageData = {
+            // Add the message data here
+        };
+
+        ws.send(JSON.stringify(messageData));
+    }
 </script>
 
-<main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-</main>
-
-<style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
-</style>
+<button on:click={connectWebsocket}>Connect WebSocket</button>
+<button on:click={disconnectWebsocket}>Disconnect WebSocket</button>
+<button on:click={sendMessage}>Send Message</button>
